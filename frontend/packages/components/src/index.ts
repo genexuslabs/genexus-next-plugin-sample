91cfcedd59cd94b601f1e70e9da2fe99405542ba
Nicolás Cámera <ncamera@genexus.com>
2024-07-16 16:53:29 -0300

    Fix index.ts

--- frontend/packages/components/src/index.ts
+++ frontend/packages/components/src/index.ts
@@ -2,4 +2,4 @@ export * from "./components";
 
 // Add types from Chameleon, without importing any JS in the final bundle.
 // Don't use 'import "@genexus/chameleon-controls-library"'
-typeof import("@genexus/chameleon-controls-library/types/index");
+typeof import("@genexus/chameleon-controls-library/dist/types/index");
