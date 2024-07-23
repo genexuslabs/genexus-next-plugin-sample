b6035e29ae48c58002418e02a0c187a64a7a5bf6
Nicolás Cámera <ncamera@genexus.com>
2024-07-23 14:15:36 -0300

    Export ASSETS.ts to the output build

--- frontend/packages/components/src/index.ts
+++ frontend/packages/components/src/index.ts
@@ -1,5 +1,5 @@
-export * from "./components";
-
 // Add types from Chameleon, without importing any JS in the final bundle.
 // Don't use 'import "@genexus/chameleon-controls-library"'
 typeof import("@genexus/chameleon-controls-library/dist/types/index");
+
+export { ASSETS } from "./assets/generated/ASSETS";
