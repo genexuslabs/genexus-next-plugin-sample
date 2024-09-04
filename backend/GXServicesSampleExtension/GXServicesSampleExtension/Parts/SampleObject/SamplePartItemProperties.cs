de3ce207252d4fe0d446aa20c1b87feccde1499d
DiegoScaffo <dscaffo@genexus.com>
2024-09-04 09:18:57 -0300

    Added basic structure to SamplePart

---
+++ backend/GXServicesSampleExtension/GXServicesSampleExtension/Parts/SampleObject/SamplePartItemProperties.cs
@@ -0,0 +1,8 @@
+namespace GXServicesSampleExtension.Parts.SampleObject;
+
+public class SamplePartItemProperties
+{
+    public const string NAME = "Name";
+    public const string DESCRIPTION = "Description";
+    public const string KB_OBJECT_NAME = "KBObjectName";
+}
--- backend/GXServicesSampleExtension/GXServicesSampleExtension/Parts/SamplePart.cs
+++ backend/GXServicesSampleExtension/GXServicesSampleExtension/Parts/SamplePart.cs
@@ -1,6 +1,10 @@
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Xml;
+using System.Xml.Serialization;
 using Artech.Architecture.Common.Descriptors;
 using Artech.Architecture.Common.Objects;
+using GXServicesSampleExtension.Parts.SampleObject;
 
 namespace GXServicesSampleExtension.Parts;
 
@@ -11,5 +15,44 @@ public class SamplePart: KBObjectPart
     public SamplePart(KBObject kbObject) 
         : base(typeof(SamplePart).GUID, kbObject)
     {
+        Items = new List<SamplePartItem>();
+    }
+
+    public List<SamplePartItem> Items { get; set; }
+
+    protected override byte[] SerializeData()
+    {
+        List<string> itemsXml = new List<string>();
+        foreach(SamplePartItem item in Items)
+            itemsXml.Add(item.SerializeToXml());
+        
+        XmlSerializer serializer = new XmlSerializer(itemsXml.GetType());
+        StringBuilder sb = new StringBuilder();
+        using(XmlWriter writer = XmlWriter.Create(sb))
+        {
+            serializer.Serialize(writer, itemsXml);
+        }
+        
+
+        return Encoding.UTF8.GetBytes(sb.ToString());
+    }
+
+	protected override void DeserializeData(byte[] data)
+    {
+        List<string> itemsXml = new List<string>();
+        string xml = Encoding.UTF8.GetString(data);
+        XmlSerializer serializer = new XmlSerializer(itemsXml.GetType());
+        using (TextReader reader = new StringReader(xml))
+        {
+            itemsXml = (serializer.Deserialize(reader) as List<string>) ?? new List<string>();
+        }
+        
+        Items = new List<SamplePartItem>();
+        foreach(string itemXml in itemsXml)
+        {
+            SamplePartItem item = new SamplePartItem(this);
+            item.DeserializeFromXml(itemXml);
+            Items.Add(item);
+        }
     }
 }
