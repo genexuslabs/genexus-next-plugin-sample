b7f508a06312348c856be066d3b4e1d6e80d9c13
DiegoScaffo <dscaffo@genexus.com>
2024-09-05 15:48:29 -0300

    Set base url for locale fetch

--- frontend/packages/main/src/contributions/register-assets.ts
+++ frontend/packages/main/src/contributions/register-assets.ts
@@ -1,5 +1,6 @@
 import { ASSETS } from "@genexusm-plugin-sample/components";
 import { AssetsManager  } from "@genexusm-sdk/common-components";
+import { Locale } from "@genexusm-plugin-sample/components";
 
 export function registerAssets(){
     AssetsManager.registerAssets('sample-vendor', 'sv', ASSETS);
@@ -7,4 +8,6 @@ export function registerAssets(){
     const baseUrl = AssetsManager.getBundleBaseUrl('PluginSample');
     const SAMPLE_BUNDLE = [{ name: 'sample/global', url: `${baseUrl}global.css`, themeBaseUrl: baseUrl }];
     AssetsManager.registerStylesBundle(SAMPLE_BUNDLE);
+
+    Locale.commonAssetsPath = baseUrl;
 }
